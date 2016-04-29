26ef34d5cf0e7932b48abe9855c50aa0bca8ba7f
visualmehedi <imehedi43@gmail.com>
2016-04-29 22:02:50 +0200

    first commit

---
+++ DemoGeoLocation/Model/AppLocation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViewModelBase2;
+
+namespace DemoGeoLocation.Model
+{
+    public class AppLocation : PropertyChangedBase
+    {
+        public string latitude;
+        public string longitude;
+        public string accuracy;
+        public string Latitude 
+        { 
+            get { return this.latitude; }
+            set { this.latitude = value;
+            OnPropertyChanged();
+            } 
+        }
+        public string Longitude
+        {
+            get { return this.longitude; }
+            set { this.longitude = value;
+            OnPropertyChanged();
+            }
+        }
+        public string Accuracy
+        {
+            get { return this.accuracy; }
+            set { this.accuracy = value;
+            OnPropertyChanged();
+            }
+        }
+
+    }
+}
---
+++ DemoGeoLocation/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoGeoLocation.Model;
+using Windows.Devices.Geolocation;
+
+namespace DemoGeoLocation.ViewModel
+{
+    public class MainPageViewModel
+    {
+        private Geolocator geo;
+        public AppLocation AppLocation { get; set; }
+
+        public MainPageViewModel()
+        {
+            AppLocation = new AppLocation();
+            geo = new Geolocator();
+        }
+        public async void UpdateLocation()
+        {
+            Geoposition pos = await geo.GetGeopositionAsync();
+            AppLocation.Latitude = pos.Coordinate.Point.Position.Latitude.ToString();
+            AppLocation.Longitude = pos.Coordinate.Point.Position.Longitude.ToString();
+            AppLocation.Accuracy = pos.Coordinate.Accuracy.ToString();
+        }
+    }
+}
